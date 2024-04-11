7ee10eec22d70aa1ea0b1c28b5858ad7e7a7953c
katt ens <kattayun.ens@gmail.com>
2024-04-11 18:45:21 -0400

    proteindataset removed max_len

--- DatasetClass.py
+++ DatasetClass.py
@@ -15,99 +15,63 @@ The class is initialized with a pandas DataFrame containing protein sequences, a
 
 """
 
-
-
 import torch
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
-import random
-
-
+import numpy as np
 
 class ProteinInteractionDataset(Dataset):
-    def __init__(self, dataframe, tokenizer, max_length=512, mask_probability=0.15):
-        """
-        Initializes the dataset.
-        :param dataframe: Pandas DataFrame containing global and local sequences.
-        :param tokenizer: Initialized BertTokenizer for sequence tokenization.
-        :param max_length: Maximum sequence length for tokenization.
-        :param mask_probability: Probability of masking a token in the local sequences.
-        """
+    def __init__(self, dataframe, tokenizer, mask_probability=0.15):
         self.dataframe = dataframe
         self.tokenizer = tokenizer
-        self.max_length = max_length
         self.mask_probability = mask_probability
 
     def __len__(self):
-        """Returns the total number of items in the dataset."""
         return len(self.dataframe)
 
     def __getitem__(self, idx):
-        """
-        Retrieves an item by index.
-        :param idx: Index of the item.
-        :return: A dictionary containing tokenized inputs for global sequences, and
-                 tokenized and dynamically masked inputs along with labels for local sequences.
-        """
         row = self.dataframe.iloc[idx]
-        # Tokenize and concatenate global sequences
-        global_seq = f"[ENTITY1] {row['Sequence_A']} [SEP] [ENTITY2] {row['Sequence_B']}"
-        local_seq = f"[ENTITY1] {row['masked_sequence_A']} [SEP] [ENTITY2] {row['masked_sequence_B']}"
-   
-        input_ids_global, attention_mask_global = self.tokenize_sequence(global_seq)
-        input_ids_local, attention_mask_local, labels_local = self.mask_and_tokenize_sequence(local_seq)
 
+        # Global sequence with random masking
+        global_seq = f"[ENTITY1] {row['Sequence_A']} [SEP] [ENTITY2] {row['Sequence_B']}"
+        input_ids_global, attention_mask_global, labels_global = self.random_mask_sequence(global_seq)
+
+        # Local sequence without masking
+        local_seq = f"[ENTITY1] {row['Sequence_A']} [SEP] [ENTITY2] {row['Sequence_B']}"
+        input_ids_local, attention_mask_local = self.tokenize_sequence(local_seq)
 
         return {
-            "input_ids_global": input_ids_global,
-            "attention_mask_global": attention_mask_global,
-            "input_ids_local": input_ids_local,
-            "attention_mask_local": attention_mask_local,
-            "labels_local": labels_local,
+            "input_ids_global": torch.tensor(input_ids_global),
+            "attention_mask_global": torch.tensor(attention_mask_global),
+            "labels_global": torch.tensor(labels_global),
+            "input_ids_local": torch.tensor(input_ids_local),
+            "attention_mask_local": torch.tensor(attention_mask_local)
         }
 
     def tokenize_sequence(self, sequence):
-        """
-        Tokenizes a sequence, respecting the max_length constraint.
-        """
         encoded = self.tokenizer.encode_plus(
             sequence,
             add_special_tokens=True,
-            max_length=self.max_length,
-            padding='max_length',
-            truncation=True,
-            return_tensors='pt'
-        )
-        return encoded['input_ids'].squeeze(0), encoded['attention_mask'].squeeze(0)
-
-
-
-    def mask_and_tokenize_sequence(self, sequence):
-        """
-        Masks tokens in a sequence with a specified probability and tokenizes the sequence.
-        """
-        tokens = self.tokenizer.tokenize(sequence)
-        masked_tokens, labels = [], []
-
-        for token in tokens:
-            if random.random() < self.mask_probability:
-                masked_tokens.append(self.tokenizer.mask_token)
-                labels.append(self.tokenizer.convert_tokens_to_ids(token))
-            else:
-                masked_tokens.append(token)
-                labels.append(-100)  # -100 is used to ignore these tokens in loss calculation
-
-        # Convert list of tokens to IDs and truncate or pad as necessary
-        encoded = self.tokenizer(
-            masked_tokens, max_length=self.max_length, padding='max_length',
-            truncation=True, is_split_into_words=True, return_tensors='pt'
+            return_tensors='pt',
+            padding=False,
+            truncation=True
         )
         input_ids = encoded['input_ids'].squeeze(0)
         attention_mask = encoded['attention_mask'].squeeze(0)
-        labels += [-100] * (self.max_length - len(labels))
+        return input_ids.numpy(), attention_mask.numpy()
 
-        return input_ids, attention_mask, torch.tensor(labels, dtype=torch.long)
-    
+    def random_mask_sequence(self, sequence):
+        """Applies random masking to a sequence as in BERT's pretraining."""
+        tokens = self.tokenizer.tokenize(sequence)
+        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+
+        # Create a mask array
+        labels = np.full(len(input_ids), -100, dtype=int)
+        mask_indices = np.random.rand(len(input_ids)) < self.mask_probability
+        labels[mask_indices] = input_ids[mask_indices]
+        input_ids[mask_indices] = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+
+        return input_ids, np.ones_like(input_ids), labels  # Attention mask is simply all ones
 
 
 
